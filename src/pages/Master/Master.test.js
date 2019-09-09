9d59fb76da2b855971b1c4559c4afe533d8e0e18
gabrielnogueira <gabrielnogueira@frwk.com.br>
2019-09-09 16:35:41 -0300

    setup test environment

---
+++ src/pages/Master/Master.test.js
@@ -0,0 +1,7 @@
+import Master from './Master';
+
+it('fetch characters with search text', () => {
+//   const comp = shallow(<Master />);
+//   comp.find('AppBar').simulate('onChange', 'hulk');
+//   expect(comp.props().searchText).toEqual('hulk');
+});
